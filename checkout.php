202ca6a109b809af99e03ba7042df0b1a4426bde
C0soo <mgrlnz05e03e507o@iisbadoni.edu.it>
2024-05-22 10:45:37 +0200

    ada

--- checkout.php
+++ checkout.php
@@ -23,7 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     // Insert the order details into the database
 
-    //$caca = json_encode($cart);
+    $caca = json_encode($cart);
     //print($caca);
 
     $sql = "INSERT INTO orders (order_details, total) VALUES ('" . json_encode($cart) . "', '$total')";
